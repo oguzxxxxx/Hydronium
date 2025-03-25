163c513d7123f80aa81103cd8c1d31b9294348e1
oguzxxxxx <originalpidorases@gmail.com>
2025-03-25 23:12:29 +0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -11,19 +11,6 @@ local user = "nunera" -- change if you're using a fork
 local branch = "revision"
 local importCache = {}
 
-
-
---attempting adonis bypasses
-for k, v in pairs(getgc(true)) do
-    if pcall(function() return rawget(v, "indexInstance") end) and
-        type(rawget(v, "indexInstance")) == "table" and
-        (rawget(v, "indexInstance"))[1] == "kick" then
-        v.tvk = {"kick", function()
-            return game.Workspace:WaitForChild("")
-        end}
-    end
-end
-
 local Namecall
 
 Namecall = hookmetamethod(game, '__namecall', function(self, ...)
